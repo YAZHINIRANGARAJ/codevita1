5e818ebd56762fa527532d9ca2f38f38a19e62a7
YAZHINI RANGARAJ <yazhinirangaraj@gmail.com>
2017-07-27 21:20:53 +0530

    Create num in pyramid.java

---
+++ num in pyramid.java
@@ -0,0 +1,28 @@
+class  Pyramid
+ 
+{
+ 
+public static void main(String[] args)
+ 
+{
+ 
+for(int i=1; i<= 5 ;i++)//to print  i values of  5 rows in increasing order
+ 
+{
+ 
+for(int j=0; j < i; j++)//to print j values in 5 columns
+ 
+{
+ 
+System.out.print(j+1);
+ 
+}
+ 
+System.out.println("");
+ 
+}
+ 
+}
+ 
+}
+
